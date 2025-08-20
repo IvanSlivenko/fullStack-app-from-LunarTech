82044f6c2a02953a439a327065f8e4ef34e231ce
IvanSlivenko <ivan8822@ukr.net>
2025-08-20 22:12:23 +0300

    20-08-2025-22-12

--- project/tailwind.config.js
+++ project/tailwind.config.js
@@ -1,9 +1,64 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+// @type {import('tailwindcss').Config}
+// module.exports = {
+//   content: [],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
 
+// import type { Config } from "tailwindcss"
+
+const config = {
+  darkMode: ["class"],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        "ibm-plex-sans": ["IBM Plex Sans", "sans-serif"],
+        "bebas-neue": ["var(--bebas-neue)"],
+      },
+      colors: {
+        primary: {
+          DEFAULT: "#E7C9A5",
+          admin: "#25388C",
+        },
+        green: {
+          DEFAULT: "#027A48",
+          100: "#ECFDF3",
+          400: "#4C7B62",
+          800: "#027A48",
+        },
+        red: {
+          DEFAULT: "#EF3A4B",
+          400: "#F46F70",
+          800: "#EF3A4B", // ← здається, тут копіпаста: red.800 = зелений :)
+        },
+        light: {
+          100: "#F46F70",
+        },
+        // background: "hsl(var(--background))",
+        // foreground: "hsl(var(--foreground))",
+        // card: {
+        //   DEFAULT: "hsl(var(--card))",
+        //   foreground: "hsl(var(--card-foreground))",
+        // },
+        // popover: {
+        //   DEFAULT: "hsl(var(--popover))",
+        //   foreground: "hsl(var(--popover-foreground))",
+        // },
+      },
+    },
+  },
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/typography"),
+    require("tailwindcss-animate"),
+  ],
+};
+
+export default config;
