597cca1f3988302fac8dde4e48ce524e31528975
IvanSlivenko <ivan8822@ukr.net>
2025-08-27 21:10:20 +0300

    27-08-2025-21-10

--- project/components/AuthForm.tsx
+++ project/components/AuthForm.tsx
@@ -1,37 +1,79 @@
 "use client";
 
 import { zodResolver } from "@hookform/resolvers/zod";
-import { DefaultValues ,FieldValues,SubmitHandler,useForm, UseFormReturn } from "react-hook-form";
+import {
+  DefaultValues,
+  FieldValues,
+  SubmitHandler,
+  useForm,
+  UseFormReturn,
+} from "react-hook-form";
 import z, { ZodType } from "zod";
+
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
 
 interface Props<T extends FieldValues> {
   schema: ZodType<T>;
   defaultValues: T;
-  onSubmit: (data: T) => Promise<{success: boolean; error?: string}>;
+  onSubmit: (data: T) => Promise<{ success: boolean; error?: string }>;
   type: "SIGN_IN" | "SIGN_UP";
 }
 
+const AuthForm = <T extends FieldValues>({
+  type,
+  schema,
+  defaultValues,
+  onSubmit,
+}: Props<T>) => {
+  const isSignIn = type === "SIGN_IN";
 
-
-const AuthForm=<T extends FieldValues>  ({ 
-  type, 
-  schema, 
-  defaultValues, 
-  onSubmit }: Props<T>) => {
-
-    const form: UseFormReturn<T> = useForm({  
+  const form: UseFormReturn<T> = useForm({
     resolver: zodResolver(schema),
-    defaultValues: defaultValues as DefaultValues<T>
-  })
- 
-  const handleSubmit: SubmitHandler<T> = async (data) => {
- 
-  }
+    defaultValues: defaultValues as DefaultValues<T>,
+  });
 
-  return <div>AuthForm --{type}</div>;
+  const handleSubmit: SubmitHandler<T> = async (data) => {};
+
+  return (
+    <div className="flex flex-col gap-4">
+      <h1 className="text-2xl font-semibold text-white">
+        {isSignIn ? "Welome back to BookWise" : "Create your library account"}
+      </h1>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input placeholder="shadcn" {...field} />
+                </FormControl>
+                <FormDescription>
+                  This is your public display name.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit" className="bg-yellow-600 text-yellow-900">
+            Submit
+          </Button>
+        </form>
+      </Form>
+    </div>
+  );
 };
 
 export default AuthForm;
-
-
-
