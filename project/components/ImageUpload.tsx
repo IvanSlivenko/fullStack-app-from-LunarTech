8315b272441eff27da78ad722d17648afedea18e
IvanSlivenko <ivan8822@ukr.net>
2025-08-29 17:24:09 +0300

    29-08-2025-17-24

--- project/components/ImageUpload.tsx
+++ project/components/ImageUpload.tsx
@@ -1,4 +1,22 @@
+"use client"
+
 import React from 'react'
+// import { Image, Video, ImageKitProvider, upload ,ImageKitContext } from '@imagekit/next';
+// import { Image, Video, ImageKitProvider } from "@imagekit/next";
+import { ImageKitProvider, IKUpload, IKImage } from "imagekitio-next";
+
+const authenticator = async ()=>{
+  try{
+const response = await fetch("/api/auth");
+    if (!response.ok) throw new Error("Auth request failed");
+    return await response.json();
+  }catch (error: any){
+    throw new Error(`Authentication request failed ${error.message}`); 
+  }
+
+}
+
+
 
 const ImageUpload = () => {
   return (
---
+++ project/lib/config.ts
@@ -0,0 +1,12 @@
+const config = {
+    env: {
+        imageKit: {
+            publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_KEY,
+            urlEndpoint: process.env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT,
+            privateKey: process.env.IMAGEKIT_PRIVATE_KEY
+                
+        }
+    }
+}
+
+export default config;
