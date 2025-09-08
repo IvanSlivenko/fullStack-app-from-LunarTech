2efe9719d540b9d634941269a4621f5e19f64641
IvanSlivenko <ivan8822@ukr.net>
2025-09-08 16:05:07 +0300

    08-09-2025-16-05

--- project/lib/workflow.ts
+++ project/lib/workflow.ts
@@ -1,7 +1,35 @@
-import { Client as Workflowclient} from "@upstash/workflow"
-import config from "@/lib/config"
+import { Client as Workflowclient } from "@upstash/workflow";
+import { Client as QStashClient, resend } from "@upstash/qstash";
+import config from "@/lib/config";
 
 export const workflowclient = new Workflowclient({
-    baseUrl: config.env.upstash.qstashUrl,
-    token: config.env.upstash.qstashToken
-})
+  baseUrl: config.env.upstash.qstashUrl,
+  token: config.env.upstash.qstashToken,
+});
+
+const qstashClient = new QStashClient({
+  token: config.env.upstash.qstashToken,
+});
+
+export const sendEmail = async ({
+  email,
+  subject,
+  message,
+}: {
+  email: string;
+  subject: string;
+  message: string;
+}) => {
+  await qstashClient.publishJSON({
+    api: {
+      name: "email",
+      provider: resend({ token: config.env.resendToken }),
+    },
+    body: {
+      from: "UmanProger <hello.umanproger@ukr.net>",
+      to: [email],
+      subject: subject,
+      html: message,
+    },
+  });
+};
